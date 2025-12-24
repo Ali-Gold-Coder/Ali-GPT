5ec50859cdd97661227035a1c35fb34ad0894463
Ali <109255594+Ali-Gold-Coder@users.noreply.github.com>
2025-12-24 15:49:04 -0400

    added the main backend server file

---
+++ Ali-GPT/frontend/src/pages/Signup.jsx
@@ -0,0 +1,20 @@
+import { useState } from "react"
+import "./Signup-Login.css";
+
+
+export default function SignUp ({ onSignup}){
+
+  const []
+    return (
+        <div className= "signup-page">
+        
+            
+
+            <button className="landing-button signup animated-text-Right " onClick={onSignup}>
+                    Sign Up
+                </button>
+
+             
+        </div>
+    );
+}
